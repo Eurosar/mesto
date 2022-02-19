5fab2e307c873b38e27afc7656c693589968234c
Eurosar <euro.saratov@gmail.com>
2022-02-19 15:32:35 +0300

    feat: создать popup добавления фотографий и удаления фотографий с сайта

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,52 @@
-const popup = document.querySelector('.popup');
+const popupProfileEditor = document.querySelector('.popup__profile-editor');
 const profileEditButton = document.querySelector('.profile__edit-button');
+const popupAddPlaces = document.querySelector('.popup__add-places');
 const profileAddButton = document.querySelector('.profile__add-button');
-const popupClose = popup.querySelector('.popup__close');
-const formElement = popup.querySelector('.popup__form');
+const popupCloseProfileEditor = document.querySelector('.close-profile-editor');
+const popupCloseAddPlaces = document.querySelector('.close-add-places');
+const formProfileEditElement = document.querySelector('.form-profile-editor');
+const formProfileAddElement = document.querySelector('.form-add-places');
+const placesList = document.querySelector('.places__list');
+const templatePlace = document.querySelector('.place-template');
 
-//Обработчик данных input в popup
-const nameInput = formElement.querySelector('.popup__input_text_name');
-const jobInput = formElement.querySelector('.popup__input_text_job');
+//Обработчик данных input в popup edit
+const nameInput = formProfileEditElement.querySelector('.popup__input_text_name');
+const jobInput = formProfileEditElement.querySelector('.popup__input_text_job');
 const nameProfile = document.querySelector('.profile__name');
 const jobProfile = document.querySelector('.profile__job');
+
+//Обработчик данных input в popup add place
+const namePlaceInput = formProfileAddElement.querySelector('.popup__input_text_name-place');
+const imageLinkInput = formProfileAddElement.querySelector('.popup__input_text_image-link');
+
+//Массив карточек мест из коробки
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
 
 // Присвоение input значений из блока profile
 function assignInputsValue() {
@@ -21,32 +59,110 @@ function assignTextContentFromInputs() {
     nameProfile.textContent = nameInput.value;
     jobProfile.textContent = jobInput.value;
 }
-//Присвоение класса popup для открытия
-function addPopupOpened() {
+
+// Создаем карточку изображения с названием в блоке Places
+function createPlaceElement(item) {
+    // Клонируем шаблон
+    const placeElement = templatePlace.content.cloneNode(true);
+    placeElement.querySelector('.place__title').textContent = item.name;
+    placeElement.querySelector('.place__image').src = item.link;
+    placeElement.querySelector('.place__image').alt = item.name;
+    placeElement.querySelector('.place__favorite').addEventListener('click', handlerLikePlace);
+    placeElement.querySelector('.place__cart').addEventListener('click', handlerRemovePlace);
+    
+    return placeElement;
+
+}
+// Лайкаем фотографии
+function handlerLikePlace(event) {
+    const placeElement = event.target.closest('.place__favorite');
+    placeElement.classList.toggle('place__favorite_active');
+}
+
+// Удаляем карточки Place со страницы по нажатию на корзину
+function handlerRemovePlace(event) {
+    const placeElement = event.target.closest('.place');
+    placeElement.remove();
+}
+
+// Перебираем массив карточек и выводим их на странице
+function arrayPlace() {
+    initialCards.forEach((item) => {
+        const arrayPlaceElement = createPlaceElement(item);
+        placesList.append(arrayPlaceElement);
+    });
+
+}
+
+arrayPlace();
+
+// Присваиваем textContent и src из input в блок Place
+function assignContentPlaceInputs() {
+    const item = {
+        name: namePlaceInput.value,
+        link: imageLinkInput.value
+    };
+    const placeCard = createPlaceElement(item);
+    placesList.prepend(placeCard);
+
+}
+
+// Выводим новую карточку в блоке Places
+function handlerSubmitFormAddPlaces(e) {
+    e.preventDefault();
+    assignContentPlaceInputs();
+    cleanOutPlaceInputs();
+    removePopup(popupAddPlaces);
+}
+
+// Очищаем inputs в handlerSubmitFormAddPlaces
+function cleanOutPlaceInputs() {
+    namePlaceInput.value = '';
+    imageLinkInput.value = '';
+}
+
+// сохраняем данные Profile  на странице
+function handlerSubmitFormProfileEdit(evt) {
+    evt.preventDefault();
+    assignTextContentFromInputs();
+    removePopup(popupProfileEditor);
+}
+
+//Присвоение класса для открытия popup
+function openPopup(popup) {
     popup.classList.add('popup_opened');
 }
 
-//Удаление класса popup для закрытия
-function removePopupOpened() {
+//Удаление класса для закрытия popup
+function removePopup(popup) {
     popup.classList.remove('popup_opened');
 }
 
-// сохраняем данные на странице
-function formProfileEditSubmitHandler(evt) {
-    evt.preventDefault();
-    assignTextContentFromInputs();
-    removePopupOpened();
-}
+//Закрытие popups
 
-//Закрытие popup
-popupClose.addEventListener('click', removePopupOpened);
+//--Закрытие Profile
+popupCloseProfileEditor.addEventListener('click', () => {
+    removePopup(popupProfileEditor);
+});
 
-//Открытие popup edit
+//--Закрытие Place
+popupCloseAddPlaces.addEventListener('click', () => {
+    removePopup(popupAddPlaces);
+});
+
+//Открытие popups
+//--Открытие Profile
 profileEditButton.addEventListener('click', () => {
-    addPopupOpened();
+    openPopup(popupProfileEditor);
     assignInputsValue();
 });
 
-// жмем кнопку сохранить данные в попапе
-formElement.addEventListener('submit', formProfileEditSubmitHandler);
+//--Открытие Place
+profileAddButton.addEventListener('click', () => {
+    openPopup(popupAddPlaces);
+});
+
+// жмем кнопку сохранить данные в попапах
+formProfileEditElement.addEventListener('submit', handlerSubmitFormProfileEdit);
+formProfileAddElement.addEventListener('submit', handlerSubmitFormAddPlaces);
 
